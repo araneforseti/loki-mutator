7cf0d30c5bb83a6d82cce088463183b018522592
Sarah Hutchins <974016+araneforseti@users.noreply.github.com>
2023-04-15 08:11:46 +0200

    Removing auto failing test

--- spec/loki/mutator_spec.rb
+++ spec/loki/mutator_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Loki::Mutator do
   it "has a version number" do
     expect(Loki::Mutator::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
